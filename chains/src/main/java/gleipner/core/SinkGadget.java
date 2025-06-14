7525b57acb5cdef3949a059847c35ccb20d2e316
brunok-cs <147604275+brunok-cs@users.noreply.github.com>
2025-06-14 07:31:35 +0200

    Update SinkGadget.java

--- chains/src/main/java/gleipner/core/SinkGadget.java
+++ chains/src/main/java/gleipner/core/SinkGadget.java
@@ -9,8 +9,7 @@ public class SinkGadget implements Serializable {
     public SinkGadget() {
     }
 
-    public void sinkMethod(String taint) {
-        // we do this to have the compiler compile, even though the invokeSinkMethod is known to throw an Exception
+    public void sinkMethod(String taint) {        
         Controller.invokeSink(taint);
     }
 
